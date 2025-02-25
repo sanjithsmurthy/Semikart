bfcb4c081676813fc9ee3a5d456fee4319c07f1a
sanjanahpoojary <sanjanahpoojary@gmail.com>
2025-02-25 11:19:05 +0530

    pubspec.yaml

---
+++ Flutter/semikart/lib/Components/LoginPage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(LoginScreen());
+}
+class LoginScreen extends StatelessWidget 
+{
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    
+      
+    return Scaffold(
+      body: SingleChildScrolllView(
+        child: Padding(
+
+
+
+        ),
+      ),
+    );
+  }
+} 
