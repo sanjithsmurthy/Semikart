ffae16fa8d662bf25fd012b42e4f8f4b85fc34b8
sanjanahpoojary <sanjanahpoojary@gmail.com>
2025-03-19 12:39:50 +0530

    Add test layouts for CustomTextField component and refactor main app structure

--- Flutter/semikart/lib/Components/Login_SignUp/LoginOTP.dart
+++ Flutter/semikart/lib/Components/Login_SignUp/LoginOTP.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'custom_text_field.dart'
 
 class loginwithpassword extends StatelessWidget{
 
   @override
-  Widget build(BuildContext context){
-
-  }
 }
---
+++ Flutter/semikart/lib/Components/trial/sanjana.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../Login_SignUp/custom_text_field.dart';
+
+class TestLayoutSanjana extends StatefulWidget {
+  @override
+  State<TestLayoutSanjana> createState() => _TestLayoutSanjanaState();
+}
+
+class _TestLayoutSanjanaState extends State<TestLayoutSanjana> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Custom TextField Test'),
+        backgroundColor: Color(0xFFA51414),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextField(
+                controller: _textController,
+                label: "Email",
+              ),
+              // Add more components to test here
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
