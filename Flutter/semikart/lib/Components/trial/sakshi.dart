6e37bbc261298c8e3457adc8ce2dd14809f5912c
sakshisnetrakar <sakshisnetrakar@gmail.com>
2025-03-19 15:02:41 +0530

    Add shipping text field component and update VSCode recommendations

--- Flutter/semikart/lib/Components/trial/sakshi.dart
+++ Flutter/semikart/lib/Components/trial/sakshi.dart
@@ -1,42 +1,16 @@
 import 'package:flutter/material.dart';
-import '../Login_SignUp/custom_text_field.dart';  // Add this import
+import '../shippingtextfield.dart'; // Import the BillingAddressScreen
 
-class TestLayoutSakshi extends StatefulWidget {
-  @override
-  State<TestLayoutSakshi> createState() => _TestLayoutSakshiState();  // Fixed class name
-}
-
-class _TestLayoutSakshiState extends State<TestLayoutSakshi> {  // Fixed class name
-  final TextEditingController _testController = TextEditingController();
-
-  @override
-  void dispose() {
-    _testController.dispose();
-    super.dispose();
-  }
-
+class TestLayoutSakshi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Test Components'),
-        backgroundColor: Color(0xFFA51414),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomTextField(
-                controller: _testController,
-                label: "Test Field",
-              ),
-              // Add more components here to test
-            ],
-          ),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BillingAddressScreen(),
     );
   }
+}
+
+void main() {
+  runApp(TestLayoutSakshi());
 }
