24c62d9dea0375c45dbddf007a6488bf96eed874
sakshisnetrakar <sakshisnetrakar@gmail.com>
2025-03-19 15:43:59 +0530

    Add BillingAddressScreen and integrate SearchBar in TestLayoutSakshi

--- Flutter/semikart/lib/Components/trial/sakshi.dart
+++ Flutter/semikart/lib/Components/trial/sakshi.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
-import '../Login_SignUp/custom_text_field.dart';  // Add this import
+import '../Commons/searchbar.dart' as custom; // Import the SearchBar widget with an alias
 
-class TestLayoutSakshi extends StatefulWidget {
-  @override
-  State<TestLayoutSakshi> createState() => _TestLayoutSakshiState();  // Fixed class name
-}
-
-class _TestLayoutSakshiState extends State<TestLayoutSakshi> {  // Fixed class name
-  final TextEditingController _testController = TextEditingController();
-
-  @override
-  void dispose() {
-    _testController.dispose();
-    super.dispose();
-  }
+class TestLayoutSakshi extends StatelessWidget {
+  final TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BillingAddressScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Search Example'),
+          backgroundColor: Color(0xFFA51414),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              custom.SearchBar(searchController: searchController),
+              // Add more widgets here
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
