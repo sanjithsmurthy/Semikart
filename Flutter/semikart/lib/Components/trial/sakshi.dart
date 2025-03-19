714ff721c3cf886361819199f0517ec272356632
sanjanahpoojary <sanjanahpoojary@gmail.com>
2025-03-19 15:31:16 +0530

    commit

--- Flutter/semikart/lib/Components/trial/sakshi.dart
+++ Flutter/semikart/lib/Components/trial/sakshi.dart
@@ -27,7 +27,7 @@ class _TestLayoutSakshiState extends State<TestLayoutSakshi> {  // Fixed class n
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BillingAddressScreen(),
+      //home: BillingAddressScreen(),
     );
   }
 }
