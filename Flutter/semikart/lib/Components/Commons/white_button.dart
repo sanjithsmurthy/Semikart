16d89d7e9a9a9b6d8cfc4c8e1b5907d8f5a847da
sanjanahpoojary <sanjanahpoojary@gmail.com>
2025-03-19 16:15:38 +0530

    Add InactiveButton and WhiteButton components; update TestLayoutSakshi to comment out BillingAddressScreen

---
+++ Flutter/semikart/lib/Components/Commons/white_button.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class WhiteButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  final bool isLoading;
+  final EdgeInsetsGeometry? padding;
+
+  const WhiteButton({
+    Key? key,
+    required this.label,
+    required this.onPressed,
+    this.isLoading = false,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final screenWidth = MediaQuery.of(context).size.width;
+        final buttonWidth = screenWidth < 400 ? screenWidth * 0.4 : 139.0;
+        
+        return Container(
+          width: buttonWidth,
+          height: 33,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                spreadRadius: 0,
+                blurRadius: 4,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: ElevatedButton(
+            onPressed: isLoading ? null : onPressed,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Color(0xFF000000),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              elevation: 0,
+              padding: EdgeInsets.zero,
+            ),
+            child: isLoading
+                ? SizedBox(
+                    width: 16,
+                    height: 16,
+                    child: CircularProgressIndicator(
+                      strokeWidth: 2,
+                      valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF000000)),
+                    ),
+                  )
+                : Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      label,
+                      style: TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 16,
+                        height: 19/16,
+                        fontFamily: 'Product Sans',
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+          ),
+        );
+      },
+    );
+  }
+}
