e9db2b7b034706f6969e22838b5dc884287866cb
sanjithsmurthy <sanjithsmurthy@gmail.com>
2025-03-20 21:08:40 +0530

    Refactor Header component to implement PreferredSizeWidget; update layout and icon sizes for improved UI consistency

--- Flutter/semikart/lib/Components/trial/sanjith.dart
+++ Flutter/semikart/lib/Components/trial/sanjith.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import '../Commons/bottom_bar_home.dart'; // Import the bottom_bar_home.dart file
+import '../Commons/header.dart'; // Import the header.dart file
 
 class TestLayoutSanjith extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Header Title"), // Add your desired header title here
-        backgroundColor: Color(0xFFA51414), // Optional: Set a custom background color
-      ),
+      appBar: Header(), // Use the updated Header as the AppBar
       body: Center(
         child: Text("Main Content Goes Here"), // Replace with your main content
       ),
