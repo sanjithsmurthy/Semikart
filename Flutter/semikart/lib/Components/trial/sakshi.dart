b0dcfdb9a455d2a84b61e80f9f3ec5a01348a93f
sakshisnetrakar <sakshisnetrakar@gmail.com>
2025-03-27 11:13:06 +0530

    Add SearchBar and GreyTextBox widgets to TestLayoutSakshi

--- Flutter/semikart/lib/Components/trial/sakshi.dart
+++ Flutter/semikart/lib/Components/trial/sakshi.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-// Import the CaptchaScreen widget
+import '../Commons/searchbar.dart' as custom; // Import the SearchBar widget with an alias
+import '../Commons/grey_text_box.dart'; // Import the GreyTextBox widget
 
 class TestLayoutSakshi extends StatelessWidget {
-  const TestLayoutSakshi({super.key});
+  final TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +11,25 @@ class TestLayoutSakshi extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Captcha Example'),
+          title: Text('Grey Text Box Example'),
           backgroundColor: Color(0xFFA51414),
         ),
-       
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              custom.SearchBar(searchController: searchController), // Add the SearchBar widget
+              SizedBox(height: 20),
+              GreyTextBox(), // Add the GreyTextBox widget
+              SizedBox(height: 20),
+              Text(
+                'Other Content Goes Here',
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
