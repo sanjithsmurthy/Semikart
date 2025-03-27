0251108a166847a06cb1e8b7fc3341a3fcab26b7
sanjithsmurthy <sanjithsmurthy@gmail.com>
2025-03-27 12:42:10 +0530

    Refactor TestLayoutSakshi to use BillingAddressScreen and remove SearchBar

--- Flutter/semikart/lib/Components/trial/sakshi.dart
+++ Flutter/semikart/lib/Components/trial/sakshi.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import '../Commons/searchbar.dart' as custom; // Import the SearchBar widget with an alias
-import '../Commons/grey_text_box.dart'; // Import the GreyTextBox widget
+import '../Commons/grey_text_box.dart'; // Import the BillingAddressScreen component
 
 class TestLayoutSakshi extends StatelessWidget {
-  final TextEditingController searchController = TextEditingController();
-
-  TestLayoutSakshi({super.key});
+  const TestLayoutSakshi({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,32 +10,15 @@ class TestLayoutSakshi extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Example Page'),
-          backgroundColor: Color(0xFFA51414),
+          title: const Text('Example Page'),
+          backgroundColor: const Color(0xFFA51414),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              custom.SearchBar(searchController: searchController), // Add the SearchBar widget
-              SizedBox(height: 20),
-              // GreyTextBox(
-              //   title: 'Shipping Address',
-              //   address: 'Magadi Main Rd, next to Prasanna Theatre, Cholurpalya, Bengaluru, Karnataka 560023',
-              //   onEdit: () {
-              //     // Handle edit action here
-              //     print('Edit button pressed');
-              //   },
-              // ), // Add the GreyTextBox widget
-            ],
-          ),
-        ),
+        body: const BillingAddressScreen(), // Use the BillingAddressScreen component here
       ),
     );
   }
 }
 
 void main() {
-  runApp(TestLayoutSakshi());
+  runApp(const TestLayoutSakshi());
 }
