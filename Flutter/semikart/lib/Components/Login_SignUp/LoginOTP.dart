dc7f058d3180c95034343a7dffbbe5b1ab1b7b2f
sanjithsmurthy <sanjithsmurthy@gmail.com>
2025-03-27 10:35:28 +0530

    Add build method to LoginOTP widget for password login UI

--- Flutter/semikart/lib/Components/Login_SignUp/LoginOTP.dart
+++ Flutter/semikart/lib/Components/Login_SignUp/LoginOTP.dart
@@ -4,4 +4,11 @@ class loginwithpassword extends StatelessWidget{
   const loginwithpassword({super.key});
 
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Login with Password'),
+      ),
+    );
+  }
 }
