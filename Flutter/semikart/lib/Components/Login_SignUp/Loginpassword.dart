077b0f1fc59e4ed885639bc94f98fe82b29fc18c
sanjanahpoojary <sanjanahpoojary@gmail.com>
2025-04-03 11:06:12 +0530

    Refactor LoginPasswordScreen to use StatelessWidget and update layout; add navigation to LoginPasswordScreen in TestLayoutSanjana

--- Flutter/semikart/lib/Components/Login_SignUp/Loginpassword.dart
+++ Flutter/semikart/lib/Components/Login_SignUp/Loginpassword.dart
@@ -1,47 +1,24 @@
 import 'package:flutter/material.dart';
 
-class LoginPasswordScreen extends StatefulWidget {
-  const LoginPasswordScreen({super.key});
-
-  @override
-  _LoginPasswordScreenState createState() => _LoginPasswordScreenState();
-}
-
-class _LoginPasswordScreenState extends State<LoginPasswordScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
+class LoginPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login with Password")),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "Email"),
-              keyboardType: TextInputType.emailAddress,
+      body: Center(
+        child: Container(
+          width: 512, // Set the width to 512
+          height: 917, // Set the height to 917
+          color: Colors.white, // Set the background color to white
+          child: Center(
+            child: Text(
+              'Login Password Screen',
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: 'Product Sans',
+                color: Colors.black,
+              ),
             ),
-            SizedBox(height: 15),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: "Password"),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Implement login logic here
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Logging in...")),
-                );
-              },
-              child: Text("Login"),
-            ),
-          ],
+          ),
         ),
       ),
     );
