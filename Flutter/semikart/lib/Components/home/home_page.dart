5dfe3a8236d65a7899feacf42c3782b0467131af
sanjithsmurthy <sanjithsmurthy@gmail.com>
2025-04-10 13:16:42 +0530

    fix: Update HomePage layout and content for improved user experience

--- Flutter/semikart/lib/Components/home/home_page.dart
+++ Flutter/semikart/lib/Components/home/home_page.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import '../common/header.dart'; // Import Header
+import '../common/bottom_bar.dart'; // Import BottomNavBar
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white, // Set the background color to white
-      body: Center(
-        child: Text(
-          "Welcome to Home Page", // Optional text
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+    return SafeArea(
+      child: Scaffold(
+        appBar: const Header(
+          onLogoTap: null, // You can define a callback if needed
+        ),
+        body: Center(
+          child: Text(
+            'Home Page Content',
+            style: Theme.of(context).textTheme.titleLarge,
           ),
         ),
+        bottomNavigationBar: const BottomNavBar(), // Use BottomNavBar as the bottom bar
       ),
     );
   }
