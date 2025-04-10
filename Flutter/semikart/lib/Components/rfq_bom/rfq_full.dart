f111160e51e4605ae7041cbe8e76e3eec62e51a1
som <tejuswamy235@gmail.com>
2025-04-10 12:05:23 +0530

    edited req full page

--- Flutter/semikart/lib/Components/rfq_bom/rfq_full.dart
+++ Flutter/semikart/lib/Components/rfq_bom/rfq_full.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'upload_file.dart'; // Import the CustomSquare component
 import 'RFQ_text_component.dart'; // Import the RFQTextComponent
 import 'rfq_adress_details.dart'; // Import the RFQAddressDetails component
-import '../common/header_withback.dart'; // Import your Header component
-import '../common/bottom_bar.dart'; // Import your BottomBar component
 
 class RFQFullPage extends StatelessWidget {
   const RFQFullPage({super.key});
@@ -13,12 +11,6 @@ class RFQFullPage extends StatelessWidget {
     return Scaffold(
       // Set the background color of the screen to white
       backgroundColor: Colors.white,
-
-      // Add the AppBar using your HeaderWithBack widget
-      appBar: HeaderWithBack(
-        // You might need to pass parameters like title here, e.g.:
-        // title: 'Request For Quote',
-      ),
 
       // Main body content
       body: SingleChildScrollView(
@@ -45,14 +37,11 @@ class RFQFullPage extends StatelessWidget {
                 },
               ),
 
-              const SizedBox(height: 20), // Add some space at the bottom if needed before the bottom bar
+              const SizedBox(height: 20),
             ],
           ),
         ),
       ),
-
-      // Add the BottomNavigationBar using your BottomBar widget
-      bottomNavigationBar: BottomNavBar(), // Instantiate your BottomBar widget
     );
   }
 }
