b18da402bc5639ea6371eb06060de8cbe5ce2c3c
sanjithsmurthy <sanjithsmurthy@gmail.com>
2025-04-17 17:24:30 +0530

    refactor: Remove RFQ_CTA and Breadcrumbs components from ProductsHeaderContent for simplification

--- Flutter/semikart/lib/Components/products/products_static.dart
+++ Flutter/semikart/lib/Components/products/products_static.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import '../common/search_bar.dart' as custom_search; // Import the SearchBar widget
-import '../common/RFQ_CTA.dart'; // Import the RFQ_CTA widget
-import 'breadcrumbs.dart'; // Import the Breadcrumbs widget
 import 'products_l2.dart'; // Import the ProductsL2Page widget
 
 class ProductsHeaderContent extends StatelessWidget {
@@ -45,47 +43,7 @@ class ProductsHeaderContent extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
               child: custom_search.SearchBar(), // Add the SearchBar widget here
             ),
-            SizedBox(height: screenHeight * 0.02), // Add spacing before RFQ_CTA
 
-            // RFQ Component
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-              child: const RFQComponent(), // Add the RFQ_CTA widget here
-            ),
-            SizedBox(height: screenHeight * 0.001), // Further reduced spacing before breadcrumbs by 5px
-
-            // Breadcrumbs (conditionally displayed)
-            if (showBreadcrumbs)
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-                child: Breadcrumbs(
-                  items: [
-                    BreadcrumbItem(
-                      label: 'Products',
-                      onTap: () => Navigator.of(context).pop(),
-                    ),
-                    BreadcrumbItem(
-                      label: 'Circuit Protection',
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ProductsL2Page()),
-                        );
-                      },
-                    ),
-                    if (showL2ComponentBreadcrumb) // Conditionally show 'L2 Component 1'
-                      BreadcrumbItem(
-                        label: 'L2 Component 1',
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const ProductsL2Page()),
-                          );
-                        },
-                      ),
-                  ],
-                ), // Add the Breadcrumbs widget here
-              ),
           ],
         ),
       ),
